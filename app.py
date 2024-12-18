c04738389b5a031c5d19438af9be12c7fe7ac9bd
supra2001 <supratim.2127@gmail.com>
2024-12-18 22:15:55 +0530

    committing again part 2

--- app.py
+++ app.py
@@ -6,8 +6,5 @@ def substract(a,b):
 
 def message():
     print('This is a temporary message')
-<<<<<<< HEAD
-    
+
 print("Last Message signing off")
-=======
->>>>>>> 30a42e61b307d5d73b912a3d5d59392b9fc17de8
